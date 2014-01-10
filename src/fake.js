9d8e5a54ea2589ee5364712de8ea308cb19caefd
Josh Jones <josh@smartbombinteractive.com>
2014-01-10 14:50:45 -0700

    some initial files to diff on

---
+++ src/fake.js
@@ -0,0 +1,14 @@
+// test source code for git practice
+
+function person(firstname,lastname,age,eyecolor)
+{
+this.firstname=firstname;
+this.lastname=lastname;
+this.age=age;
+this.eyecolor=eyecolor;
+}
+
+myFather=new person("John","Doe",50,"blue");
+
+document.write(myFather.firstname + " is " + myFather.age + " years old.");
+
